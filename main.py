08ab238f9219bf59fbda38e70055f96d9c127734
Shannon <skomguem@gmail.com>
2018-07-29 18:47:59 -0700

    Created Handlers

---
+++ main.py
@@ -0,0 +1,29 @@
+import webapp2
+
+# First game page
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        #stuff
+
+# Second game page
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        #stuff
+
+# Third game page
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        #stuff
+
+# FOurth game page
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        #stuff
+
+# Intitialize webpages:
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/section2', SecondPage),
+    ('/section3', ThirdPage),
+    ('/section4', FourthPage),
+], debug=True)
