ff844fe58ec7fa58a350c7ff467466af3bcc638c
Shannon <skomguem@gmail.com>
2018-07-29 19:10:57 -0700

    Connect python to HTML file

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+# AI or You main python file
 import webapp2
+import jinja2
 
+# Create Jinja environment templates
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 # First game page
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        #stuff
+        # Link Handler to webpage
+        MainPageTemp = JINJA_ENVIRONMENT.get_template('templates/game1.html')
 
 # Second game page
 class MainPage(webapp2.RequestHandler):
@@ -26,5 +34,4 @@ app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/section2', SecondPage),
     ('/section3', ThirdPage),
-    ('/section4', FourthPage),
 ], debug=True)
