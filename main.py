da99b416ec56c2e9eddc43f2835632e86f405752
Shannon <skomguem@gmail.com>
2018-07-30 09:00:21 -0700

    updated files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # AI or You main python file
 import webapp2
 import jinja2
+import os
 
 # Create Jinja environment templates
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -14,15 +15,31 @@ class MainPage(webapp2.RequestHandler):
         # Link Handler to webpage
         MainPageTemp = JINJA_ENVIRONMENT.get_template('templates/game1.html')
 
+        dicti = {'number': 0}
+        for i in range(5):
+            dicti['number'] = i
+            # Render page using template
+            self.response.write(MainPageTemp.render(dicti))
+
 # Second game page
 class SecondPage(webapp2.RequestHandler):
     def get(self):
-        #stuff
+        # Link Handeler to webpage
+        SecPageTemp = JINJA_ENVIRONMENT.get_template('templates/game2.html')
+
+        # Render page using template
+        self.response.write(SecPageTemp.render())
 
 # Third game page
 class ThirdPage(webapp2.RequestHandler):
     def get(self):
-        #stuff
+        # Link Handler to webpage
+        ThirdPageTemp = JINJA_ENVIRONMENT.get_template('templates/game3.html')
+
+        # Render page using template
+        self.response.write(ThirdPageTemp.render())
+
+
 
 
 # Intitialize webpages:
